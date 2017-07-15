4d35ee08c7f090fdccdcfe79d08a013c07a447c3
ashmeet <ashmeetsingh2010@gmail.com>
2017-07-15 19:13:01 +0000

    second

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 //
-// # SimpleServer
+// # SimplestServer
 //
-// A simple chat server using Socket.IO, Express, and Async.
-//
+// by Rick Kozak
+
+//require statements -- this adds external modules from node_modules or our own defined modules
 var http = require('http');
 var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var mongoose = require('mongoose');
+var Post = require('./models/Post.js');
+
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -18,67 +19,79 @@ var express = require('express');
 //
 var router = express();
 var server = http.createServer(router);
-var io = socketio.listen(server);
 
+//establish connection to our mongodb instance
+//use your own mongodb instance here
+//mongoose.connect('mongodb://user8165:pswd8165@ds161021.mlab.com:61021/prog8165');
+
+mongoose.connect('mongodb://navi:navi@ds129402.mlab.com:29402/instagramclonenavi');
+
+//tell the router (ie. express) where to find static files
 router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+//tell the router to parse JSON data for us and put it into req.body
+router.use(express.bodyParser());
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
+//tell the router how to handle a get request to the root 
+router.get('/', function(req, res){
+  console.log('client requests posts.html');
+  //use sendfile to send our posts.html file
+  res.sendfile(path.join(__dirname, 'client','posts.html'));
+});
 
-    sockets.push(socket);
+//tell the router how to handle a post request to /posts
+router.post('/posts', function(req, res){
+  console.log('client requests posts list');
+  
+  //go find all the posts in the database
+  Post.find({})
+  .then(function(paths){
+    //send them to the client in JSON format
+    res.json(paths);
+  })
+  
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
+});
 
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
+//tell the router how to handle a post request to /incrLike
+router.post('/incrLike', function(req, res){
+  console.log('increment like for ' + req.body.id);
 
-      if (!text)
-        return;
+  Post.findById(req.body.id)
+  .then(function(post){
+    //increment the like count
+    post.likeCount++;
+    //save the record back to the database
+    return post.save(post);
+  })
+  .then(function(post){
+    //a successful save returns back the updated object
+    res.json({id: req.body.id, count: post.likeCount});  
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+});
 
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
+router.post('/decrLike', function(req, res){
+  console.log('decrement like for ' + req.body.id);
 
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  Post.findById(req.body.id)
+  .then(function(post){
+    //increment the like count
+    post.likeCount--;
+    //save the record back to the database
+    return post.save(post);
+  })
+  .then(function(post){
+    //a successful save returns back the updated object
+    res.json({id: req.body.id, count: post.likeCount});  
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+});
+//set up the HTTP server and start it running
+server.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log('Server listening at', addr.address + ':' + addr.port);
 });
